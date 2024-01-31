fee9bc835e07f66c4e16dd37b7ae268f39724da4
aatlic1 <aatlic1@etf.unsa.ba>
2024-01-31 14:01:54 +0100

    creating an abstract account class, interface and transaction class

---
+++ Accounts/AbstractAccount.cs
@@ -0,0 +1,16 @@
+﻿using Bank.Transactions;
+
+namespace Bank.Accounts
+{
+	public abstract class AbstractAccount()
+	{
+		private int _accountNumber;
+		private double _saldo;
+		private List<Transaction> _transactions;
+		public double AccountMaintenance { get; set; }
+		public abstract int GetAccountNumber();
+		public abstract double GetSaldo();
+		public abstract string BankStatment();
+		public abstract string MonthStatment();
+	}
+}
---
+++ Transactions/Transaction.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Bank.Transactions
+{
+	public class Transaction
+	{
+		DateTime _date;
+		string _description, _currency, _instrument;
+		int _accountNumber, _quantity;
+		double _amount, _bankingServices;
+		bool _successful;
+
+		public Transaction(DateTime date, string description, double amount)
+		{
+			this._date = date;
+			this._description = description;
+			this._amount = amount;
+			_successful = true;
+		}
+
+		public Transaction(DateTime date, string description, int accountNumber, double amount, double services)
+			: this (date, description, amount)
+		{
+			this._accountNumber = accountNumber;
+			this._bankingServices = services;
+		} 
+
+		public Transaction(DateTime date, string description, double amount, string currency)
+			: this(date, description, amount)
+		{
+			this._currency = currency;
+		}
+
+		public Transaction(DateTime date, string description, double amount, int accountNumber, string currency, double services)
+			: this(date, description, amount)
+		{
+			this._accountNumber = accountNumber;
+			this._currency = currency;
+			this._bankingServices = services;
+		}
+
+		public Transaction(DateTime date, string description, double amount, int accountNumber, string instrument, int quantity, double services)
+			: this(date, description, amount)
+		{
+			this._accountNumber = accountNumber;
+			this._instrument = instrument;
+			this._quantity = quantity;
+			this._bankingServices = services;
+		}
+		public bool Successful { get; set; }
+		public string TransactionStatement()
+		{
+			var status = _successful ? "The transaction has been successful." : "The transaction has been failed.";
+
+			if (_accountNumber != 0 && _currency != null)
+				return "Date: " + _date + "\nDescription: " + _description + "\nAmount: " + _amount + "\nServices provided by the bank" + _bankingServices +
+					"\nAccount Number: " + _accountNumber + "\nCurrency: " + _currency + "\nStatus: " + status;
+
+			if (_instrument != null && _quantity != 0)
+				return "Date: " + _date + "\nDescription: " + _description + "\nInstrument: " + _instrument + "\nQuantity: " + _quantity + 
+					"\nAmount: " + _amount + "\nServices provided by the bank" + _bankingServices + "\nAccount Number: " + _accountNumber + 
+					"\nStatus: " + status;
+
+			if (_accountNumber == 0)
+				return "Date: " + _date + "\nDescription: " + _description + "\nAmount: " + _amount + "\nServices provided by the bank" + _bankingServices +
+						"\nCurrency: " + _currency + "\nStatus: " + status;
+
+			if(_currency  == null)
+				return "Date: " + _date + "\nDescription: " + _description + "\nAmount: " + _amount + "\nServices provided by the bank" + _bankingServices +
+					"\nAccount Number: " + _accountNumber + "\nStatus: " + status;	
+
+			return "Date: " + _date + "\nDescription: " + _description + "\nAmount: " + _amount + "\nStatus: " + status;
+		}
+	}
+}
