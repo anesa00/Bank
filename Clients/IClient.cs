9279e6bb451f97df4858aa974ec797cbc59e10ed
aatlic1 <aatlic1@etf.unsa.ba>
2024-02-02 15:01:07 +0100

    changing constructors for individual clients and implementing the legal entity class

--- Clients/IClient.cs
+++ Clients/IClient.cs
@@ -8,6 +8,8 @@ namespace Bank.Clients
 {
     public interface IClient
     {
+        void OpenSavingAccount(long accountNumber, double accountMaintenance, double saldo = 0, double minSaldo = 0, double bankInterest = 0,
+            int transactionLimit = 0)
         void TakeLoan(double interestRate, int loanTerm, double principal, double insuranceAndFess, string repaymentTerms);
         void CloseLoan();
         void OpenOnlineAccount(string userName, string password);
--- Clients/LegalEntityClient.cs
+++ Clients/LegalEntityClient.cs
@@ -1,15 +1,12 @@
-﻿using Bank.Accounts;
+﻿
+
+using Bank.Accounts;
 using Bank.Cards;
 using Bank.Loans;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bank.Clients
 {
-    public class IndvidualClient : AbstractClient, IClient
+    public class Owner
     {
         private string _JMBG;
         private DateTime _birthDate;
@@ -21,13 +18,7 @@ namespace Bank.Clients
         public string PhonoNumber { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        private Loan _loan;
-        private void CheckIndex(int index)
-        {
-            if (index == -1)
-                throw new ArgumentException("There is no account with this account number!");
-        }
-        private IndvidualClient(string name, string surname, DateTime birthDate, int age, string JMBG, string adress, string phonoNumber, string email = "")
+        public Owner(string name, string surname, DateTime birthDate, int age, string JMBG, string adress, string phonoNumber, string email = "")
         {
             Name = name;
             Surname = surname;
@@ -38,26 +29,49 @@ namespace Bank.Clients
             PhonoNumber = phonoNumber;
             Email = email;
         }
-        public IndvidualClient(string name, string surname, DateTime birthDate, int age, string JMBG, string adress, string phonoNumber, long accountNumber,
-            double accountMaintenance, string email = "", double saldo = 0, double limit = 0)
-            : this(name, surname, birthDate, age, JMBG, adress, phonoNumber, email)
+    }
+    public class LegalEntityClient: AbstractClient, IClient
+    {
+        public Owner Owner { get; set; }
+        private string _id;
+        public string Adress { get; set; }
+        private Loan _loan;
+        private void CheckIndex(int index)
         {
-            OpenCurrentAccount(accountNumber, accountMaintenance, saldo, limit);
+            if (index == -1)
+                throw new ArgumentException("There is no account with this account number!");
         }
-        public IndvidualClient(string name, string surname, DateTime birthDate, int age, string JMBG, string adress, string phonoNumber, long accountNumber, 
-            double accountMaintenance, string email = "", double saldo = 0, double minSaldo = 0, double bankInterest = 0, int transactionLimit = 0) 
-            : this(name, surname, birthDate, age, JMBG, adress, phonoNumber, email)
+        private LegalEntityClient(string id, string adressOfCompany, string name, string surname, DateTime birthDate, int age, string JMBG, string adress,
+            string phonoNumber, string email = "")
+        {
+            _id = id;
+            Adress = adressOfCompany;
+            Owner = new Owner(name, surname, birthDate, age, JMBG, adress, phonoNumber, email);
+        }
+        public LegalEntityClient(string id, string adressOfCompany, string name, string surname, DateTime birthDate, int age, string JMBG, string adress,
+            double accountMaintenance, long accountNumber, string phonoNumber, string email = "", double saldo = 0, double minSaldo = 0, 
+            double bankInterest = 0, int transactionLimit = 0)
+            : this(id, adressOfCompany, name, surname, birthDate, age, JMBG, adress, phonoNumber, email)
         {
             OpenSavingAccount(accountNumber, accountMaintenance, saldo, minSaldo, bankInterest, transactionLimit);
         }
-        public void OpenCurrentAccount(long accountNumber, double accountMaintenance, double saldo = 0, double limit = 0)
+        public LegalEntityClient(string id, string adressOfCompany, string name, string surname, DateTime birthDate, int age, string JMBG, string adress,
+            double accountMaintenance, long accountNumber, string phonoNumber, string email = "", double saldo = 0, string accountCurrency = "BAM", 
+            int dailyTransactionLimit = 0, int monthlyTransactionLimit = 0, int limit = 0)
+            : this(id, adressOfCompany, name, surname, birthDate, age, JMBG, adress, phonoNumber, email)
         {
-            Accounts.Add(new CurrentAccount(accountNumber, accountMaintenance, saldo, limit));
+            OpenBusinessAccount(accountNumber, accountMaintenance, saldo, accountCurrency, dailyTransactionLimit, monthlyTransactionLimit, limit);
         }
-        public void OpenSavingAccount(long accountNumber, double accountMaintenance, double saldo = 0, double minSaldo = 0, double bankInterest = 0, 
+        public void OpenSavingAccount(long accountNumber, double accountMaintenance, double saldo = 0, double minSaldo = 0, double bankInterest = 0,
             int transactionLimit = 0)
         {
             Accounts.Add(new SavingAccount(accountNumber, accountMaintenance, saldo, minSaldo, bankInterest, transactionLimit));
+        }
+
+        public void OpenBusinessAccount(long accountNumber, double accountMaintenance, double saldo = 0, string accountCurrency = "BAM", 
+            int dailyTransactionLimit = 0, int monthlyTransactionLimit = 0, int limit = 0)
+        {
+            Accounts.Add(new BusinessAccount(accountNumber, accountMaintenance, saldo, accountCurrency, dailyTransactionLimit, monthlyTransactionLimit, limit));
         }
         public override void CloseAccount(long accountNumber)
         {
@@ -71,7 +85,7 @@ namespace Bank.Clients
 
                 throw;
             }
-            
+
             Accounts.RemoveAt(index);
         }
         public override void GetCard(CardType card, long accountNumber, long cardNumber, int pin, int cvv, DateOnly cardExpiry)
@@ -96,13 +110,13 @@ namespace Bank.Clients
         }
         public void OpenOnlineAccount(string userName, string password)
         {
-            UserName = userName;
-            Password = password;
+            Owner.UserName = userName;
+            Owner.Password = password;
         }
         public void CloseOnlineAccount()
         {
-            UserName = "";
-            Password = "";
+            Owner.UserName = "";
+            Owner.Password = "";
         }
         public void TakeLoan(double interestRate, int loanTerm, double principal, double insuranceAndFess, string repaymentTerms)
         {
